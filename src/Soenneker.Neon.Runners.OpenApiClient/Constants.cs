b9e5d6174c88cfdeafb4930572bc4ace7ce7b80b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:25 -0500

    xml commenting

--- src/Soenneker.Neon.Runners.OpenApiClient/Constants.cs
+++ src/Soenneker.Neon.Runners.OpenApiClient/Constants.cs
@@ -1,6 +1,12 @@
 namespace Soenneker.Neon.Runners.OpenApiClient;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.Neon.OpenApiClient";
 }
